f233ceb4662aca31f3bc09de7ef4fa6ece08ce7f
senwang <senwang@senwangdeMacBook-Pro.local>
2015-03-16 17:30:59 +0800

    完成分页  准备投票

--- server/fixtures.js
+++ server/fixtures.js
@@ -18,7 +18,9 @@ if (Posts.find().count() === 0) {
         author: sacha.profile.name,
         url: 'http://sachagreif.com/introducing-telescope/',
         submitted: new Date(now - 7 * 3600 * 1000),
-        commentsCount: 2
+        commentsCount: 2,
+        upvoters: [],
+        votes: 0
     });
 
     Comments.insert({
@@ -43,7 +45,9 @@ if (Posts.find().count() === 0) {
         author: tom.profile.name,
         url: 'http://meteor.com',
         submitted: new Date(now - 10 * 3600 * 1000),
-        commentsCount: 0
+        commentsCount: 0,
+        upvoters: [],
+        votes: 0
     });
 
     Posts.insert({
@@ -52,7 +56,9 @@ if (Posts.find().count() === 0) {
         author: tom.profile.name,
         url: 'http://themeteorbook.com',
         submitted: new Date(now - 12 * 3600 * 1000),
-        commentsCount: 0
+        commentsCount: 0,
+        upvoters: [],
+        votes: 0
     });
 
     for (var i = 0; i < 10; i++) {
@@ -62,7 +68,9 @@ if (Posts.find().count() === 0) {
             userId: sacha._id,
             url: 'http://google.com/?q=test-' + i,
             submitted: new Date(now - i * 3600 * 1000),
-            commentsCount: 0
+            commentsCount: 0,
+            upvoters: [],
+            votes: 0
         });
     }
 }
