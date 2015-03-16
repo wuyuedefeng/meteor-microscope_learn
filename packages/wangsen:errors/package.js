71441da437fcd4ccdb0672d84f95dd03ddf8d792
senwang <senwang@senwangdeMacBook-Pro.local>
2015-03-16 12:22:05 +0800

    自己创建package 准备测试包

---
+++ packages/wangsen:errors/package.js
@@ -0,0 +1,22 @@
+Package.describe({
+  name: 'wangsen:errors',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'A pattern to display application errors to the user',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.3.2');
+  api.addFiles('wangsen:errors.js');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('wangsen:errors');
+  api.addFiles('wangsen:errors-tests.js');
+});
